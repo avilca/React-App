5e0e59adeb0f03bd35d1ca8aa846fa4fb78097b9
avilca <avilca85@gmail.com>
2025-11-20 18:27:50 -0500

    Eliminar Tarea

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,14 @@ function App() {
     }])
   }
 
+  function deleteTask(taskId) {
+    setTasks(tasks.filter(task => task.id !== taskId))
+  }
 
   return (
     <>
       <TaskForm  createTask={createTask}/>
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks} deleteTask={deleteTask}/>
     </>
   )
 }
