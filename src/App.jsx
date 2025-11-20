5eafa03180d36b4670d149d22c54753987027d89
avilca <avilca85@gmail.com>
2025-11-20 17:52:52 -0500

    App React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import TaskList from "./components/TaskList"
+import TaskForm from "./components/TaskForm"
+import { tasks as data } from "./data/tasks";
+import { useState, useEffect } from "react";
+
+function App() {
+
+  const [tasks, setTasks] = useState([]);
+  
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+  
+
+  function createTask(task) {
+    setTasks([...tasks, {
+      title: task.title,
+      id: tasks.length,
+      description: task.description
+    }])
+  }
+
+
+  return (
+    <>
+      <TaskForm  createTask={createTask}/>
+      <TaskList tasks={tasks} />
+    </>
+  )
+}
+
+export default App
---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primer tarea',
+        description: 'Descripcion de mi primer tarea'
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'Descripcion de mi segunda tarea'
+    },
+    {
+        id: 2,
+        title: 'Mi tercera tarea',
+        description: 'Descripcion de mi tercera tarea'
+    },
+]
